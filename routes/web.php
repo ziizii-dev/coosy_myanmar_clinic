7e305a61afbbcd0f5b100e31a09b1baae9ebb757
ziizii-dev <ziizii.dev@gmail.com>
2024-04-05 08:49:51 +0630

    master

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::middleware(['auth'])->group(function(){
+
+    //login, register
+    Route::redirect('/', 'loginPage');
+    Route::get('loginPage',[AuthController::class,'loginPage'])->name('auth#loginPage');
+    Route::get('registerPage',[AuthController::class,'registerPage'])->name('auth#registerPage');
+    });
+    
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
